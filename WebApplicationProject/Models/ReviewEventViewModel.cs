233d4b1db61dcee70885dda9b2905e66204e0983
SettapongKeawnok <settapong.24050@gmail.com>
2026-03-07 23:36:46 +0700

    Add Review ViewModel
    Now Review Page
    MockData Use By Controller !

---
+++ WebApplicationProject/Models/ReviewEventViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace WebApplicationProject.Models
+{
+    public class ReviewEventViewModel
+    {
+        public Event EventData { get; set; }
+        public int CurrentLoggedInUserId { get; set; }
+        public List<ReviewTargetUserModel> TargetUsers { get; set; } = new List<ReviewTargetUserModel>();
+    }
+
+    public class ReviewTargetUserModel
+    {
+        public User UserInfo { get; set; }
+        public bool IsHost { get; set; }
+        public DateTime JoinedAt { get; set; }
+        public Review MyReviewToThisUser { get; set; }
+    }
+}
